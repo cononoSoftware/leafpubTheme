310c13b6b90125e04ee4349dd7f572e872298ee1
Patric Rudolph <p.rudolph@conono.org>
2018-07-10 14:41:29 +0200

    Version 1.0.0 Build

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,6 +4,12 @@ jQuery(document).ready(function () {
 
 window.addEventListener("load", function(){
     window.cookieconsent.initialise({
-        "position": "top"
+        "position": "top",
+        "content": {
+            "message": "Diese Webseite verwendet Cookies. Indem Sie diese Webseite nutzen erklären Sie sich mit dieser Verwendung einverstanden.",
+            "dismiss": "Hinweis ausblenden!",
+            "link": "Datenschutzerklärung",
+            "href": "/datenschutz"
+        }
     })
 });
